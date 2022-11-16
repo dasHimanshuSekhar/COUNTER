2ca7d918128c50a397563a0cc1d8c821d2bd7f80
Himanshu Sekhar Das <61196311+dasHimanshuSekhar@users.noreply.github.com>
2022-11-16 18:44:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ var bt1 = document.getElementById('btn1');
 var bt2 = document.getElementById('btn2');
 var cntr = document.getElementById('counter');
 
-var cnt = 0;
+var cnt = 1;
 bt1.addEventListener('click', count);
 bt2.addEventListener('click', reset);
 
 function reset(){
     cntr.innerHTML = 0 + " ";
-    cnt = 0;
+    cnt = 1;
 }
 
 function count(){
