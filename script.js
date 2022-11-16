dce1ec25a4480237aaf18b03898613b6a973da33
Himanshu Sekhar Das <61196311+dasHimanshuSekhar@users.noreply.github.com>
2022-11-16 18:30:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var bt1 = document.getElementById('btn1');
+var bt2 = document.getElementById('btn2');
+var cntr = document.getElementById('counter');
+
+var cnt = 0;
+bt1.addEventListener('click', count);
+bt2.addEventListener('click', reset);
+
+function reset(){
+    cntr.innerHTML = 0 + " ";
+    cnt = 0;
+}
+
+function count(){
+    cntr.innerHTML = cnt + " ";
+    cnt ++;
+}
